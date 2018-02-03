295a99d443c44f93324e6ddcb4b7621921b9a7e9
Ben Carr <benjamin.carr@sky.uk>
2018-02-03 15:45:48 +0000

    Add swig templating engine

--- rollup.config.js
+++ rollup.config.js
@@ -1,21 +1,25 @@
 // import resolve from 'rollup-plugin-node-resolve';
 // import commonjs from 'rollup-plugin-commonjs';
 // import uglify from 'rollup-plugin-uglify';
+import copy from 'rollup-plugin-copy';
 
-const production = !process.env.ROLLUP_WATCH;
+// const production = !process.env.ROLLUP_WATCH;
 
 export default {
-	input: 'src/index.js',
-	output: {
-		file: 'dist/bundle.js',
-		format: 'cjs'
-	},
-	plugins: [
-		// resolve(), // tells Rollup how to find date-fns in node_modules
-		// commonjs(), // converts date-fns to ES modules
-		// production && uglify() // minify, but only in production
+  input: 'src/index.js',
+  output: {
+    file: 'build/bundle.js',
+    format: 'cjs',
+  },
+  plugins: [
+    // resolve(), // tells Rollup how to find date-fns in node_modules
+    // commonjs(), // converts date-fns to ES modules
+    // production && uglify() // minify, but only in production
+    copy({
+      'src/template.html': 'build/index.html',
+    }),
   ],
   watch: {
-    include: 'src/**'
-  }
+    include: 'src/**',
+  },
 };
--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import Koa from 'koa';
+import swig from 'swig';
 
 const PORT = process.env.PORT || 3000;
 const app = new Koa();
 
+const template = swig.compileFile('build/index.html');
+
 app.use(async (ctx) => {
-  ctx.body = 'Hello world';
+  ctx.body = template({
+    title: 'Hello world',
+  });
 });
 
 app.listen(PORT);
